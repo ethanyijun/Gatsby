fbc33c8f3a5bbc26857c3a9e2fbc3b81d3dcd4f5
yijungai <ethan.yijun@gmail.com>
2020-07-17 17:42:11 +1000

    Add navigation component

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
+import Navigation from "./navigation"
 
 import style from "./header.module.css"
 
@@ -19,6 +20,7 @@ const Header = ({ siteTitle, siteDescription }) => (
         <div className={style.site_description}>{siteDescription}</div>
       </Link>
     </div>
+    <Navigation />
   </header>
 )
 
