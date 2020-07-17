82dd79ff56bf7055852f164ee335848054711eca
yijungai <ethan.yijun@gmail.com>
2020-07-17 23:11:27 +1000

    Add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import style from "./index.module.css"
+
+const NewWrapper = styled.div`
+  border-radius: 1rem;
+  background: #ffffff;
+  box-shadow: 1rem 1rem 3rem hsla(0, 0%, 100%, 0.2), -1rem -1rem 3rem #ffffff;
+`
 
 const IndexPage = () => {
   return (
@@ -17,13 +24,13 @@ const IndexPage = () => {
       />
       <section className={style.wrapper}>
         <h1 className={style.heading}>This is the new title for the page.</h1>
-        <div>
+        <NewWrapper>
           <p>This file, index.js, functions as the homepage for the site.</p>
           <p>
             It can be populated with pure HTML like this, and extended using all
             available Gatsby and React components.
           </p>
-        </div>
+        </NewWrapper>
       </section>
     </Layout>
   )
